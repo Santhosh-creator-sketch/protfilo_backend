83ecaa9512ebfa306372b3f7c107f896fbb4ce27
Santhosh S <santhoshs15032002@gmail.com>
2026-08-05 11:28:21 +0530

    Update Invoice.js

--- Invoice.js
+++ Invoice.js
@@ -1,9 +1,1 @@
-const mongoose = require('mongoose');
 
-const invoiceSchema = new mongoose.Schema({
-    label: { type: String, required: true },
-    state: { type: mongoose.Schema.Types.Mixed, required: true }, // full invoice form data
-    savedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Invoice', invoiceSchema);
